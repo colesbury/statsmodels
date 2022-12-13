9dc166f5ffd22e0150bedcc6aba6a19b62e67127
Sam Gross <colesbury@gmail.com>
2022-12-13 13:14:13 -0800

    nogil: update setup.py and pyproject.toml

--- setup.py
+++ setup.py
@@ -53,12 +53,12 @@ with open("requirements.txt", encoding="utf-8") as req:
     for line in req.readlines():
         INSTALL_REQUIRES.append(line.split("#")[0].strip())
 
-CYTHON_MIN_VER = "0.29.32"  # released 2020
-
 DEVELOP_REQUIRES = []
 with open("requirements-dev.txt", encoding="utf-8") as req:
     for line in req.readlines():
         DEVELOP_REQUIRES.append(line.split("#")[0].strip())
+
+CYTHON_MIN_VER = "0.29.26"  # released 2020
 
 EXTRAS_REQUIRE = {
     "build": ["cython>=" + CYTHON_MIN_VER],
@@ -97,11 +97,9 @@ CLASSIFIERS = [
     "Development Status :: 4 - Beta",
     "Environment :: Console",
     "Programming Language :: Cython",
-    "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
-    "Programming Language :: Python :: 3.11",
     "Operating System :: OS Independent",
     "Intended Audience :: End Users/Desktop",
     "Intended Audience :: Developers",
@@ -406,7 +404,7 @@ setup(
     install_requires=INSTALL_REQUIRES,
     extras_require=EXTRAS_REQUIRE,
     zip_safe=False,
-    python_requires=">=3.7",
+    python_requires=">=3.8",
 )
 
 # Clean-up copied files
